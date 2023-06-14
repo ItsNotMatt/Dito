51e68855b6c275ba3bbcf1543cace51f0bc2420f
Matt <mmancarella23@gmail.com>
2023-06-14 12:13:35 -0500

    fixed the save and load system

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 #![allow(dead_code)]
+use std::io::{Write, Read};
 use std::path::Path;
 use std::{env, path::PathBuf};
-use std::fs::File;
+use std::fs::{File, OpenOptions, metadata};
 use serde::{Serialize, Deserialize};
 use serde_json;
 
 struct TodoList {
     items: Vec<TodoItem>,
-    file: Option<File>
+    file: Option<File>,
+    path: Option<String>
 }
 
 impl TodoList {
@@ -18,11 +20,15 @@ impl TodoList {
     fn parse_args(&mut self, args: Vec<String>) {
         if args[1] == String::from("add") {
             if args.len() >= 3 {
-                self.add_command(args);        
+                self.add_item(args);        
             }
             else {
                 panic!("No command after add");
             }
+        }
+        else if args[1] == String::from("show") {
+            self.get_items();
+            self.display();
         }
 
         else {
@@ -32,27 +38,74 @@ impl TodoList {
     }
 
     //need to add to json file
-    fn add_command(&mut self, args: Vec<String>) {
-        let item = TodoItem::new(1, args[2].clone(), None, false);
-        println!("{:?}", item);
-        self.items.push(item);
+    fn add_item(&mut self, args: Vec<String>) {
+        println!("Adding");
+        self.get_items(); //brings all items from json to the struct
+                          
+        //let mut content = String::new();
+        //self.file.as_ref().expect("Cant read").read_to_string(&mut content).unwrap();
+        //println!("Content: {}", content);
+
+        let item = TodoItem::new(self.items.len() + 1, args[2].clone(), None, false);
+        self.items.push(item.clone());
+        for i in &self.items {
+            println!("items: {:?}", i);
+        }
+
+        if !self.items.is_empty() {
+            if let Ok(json) = serde_json::to_string_pretty(&self.items) {
+                //println!("{:?}", json);
+                //gotta fix this path issue to open the file with truncate true so erase everything
+                //in the file so it can be rewritten with the new content + the old content
+                let s = self.path.clone().unwrap();
+                let path = Path::new(&s);
+                self.file = Some(OpenOptions::new().write(true).truncate(true).open(&path).expect("Err opening file"));
+                self.file.as_ref().expect("Cant write to file").write_all(json.as_bytes()).unwrap();
+            }
+        }
         
     }
 
     //deserialize the json file
-    fn show(&mut self) {
+    fn get_items(&mut self) {
+        //display in completed to incomplete order
+        //display only complete or only incomplete
 
+        let mut content = String::new();
+        self.file.as_ref().expect("Cant read").read_to_string(&mut content).unwrap();
+        println!("Content: {}", content);
+        if let Ok(todos) = serde_json::from_str::<Vec<TodoItem>>(&content) {
+            println!("Deserializing Multiple: {:?}", todos);
+            for i in todos {
+                //println!("{:?}", i);
+                self.items.push(i);
+            }
+        }
+        else if let Ok(todo) = serde_json::from_str::<TodoItem>(&content) {
+            println!("Deserializing Singular: {:?}", todo);
+            self.items.push(todo);
+        }
+        else if content.is_empty() {
+            println!("No content");
+        }
+        else {
+            println!("Err getting todo item from json");
+        }
     }
 
-    fn write_to_json(&mut self, item: TodoItem) {
-//        serde_json::to_writer()
+    fn display(&mut self) {
+        println!("Showing todos");
+        for todo in &self.items {
+            println!("[X]{}: {}", todo.id, todo.title);
+        } 
     }
+
 
 }
  
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 struct TodoItem {
-    id: i32,
+    id: usize,
     title: String,
     #[serde(rename = "additionalNotes")]
     additional_notes: Option<String>,
@@ -60,7 +113,7 @@ struct TodoItem {
 }
 
 impl TodoItem {
-    fn new(id: i32, title: String, additional_notes: Option<String>, completed: bool) -> Self {
+    fn new(id: usize, title: String, additional_notes: Option<String>, completed: bool) -> Self {
         Self {
             id,
             title,
@@ -70,25 +123,17 @@ impl TodoItem {
     }
 }
 
-#[derive(PartialEq, PartialOrd)]
-enum Commands {
-    Add,
-    Complete,
-    Remove,
-    Show
-}
-
 fn find_todo(path: &Path) -> Option<File> {
     if path.exists() {
         println!("Found path!");
-        let file = File::open(path).unwrap();
+        let file = OpenOptions::new().read(true).write(true).open(path).unwrap();
         return Some(file)
     }
     None
 }
 
 fn main() {
-    let mut todo = TodoList {items: vec![], file: None};
+    let mut todo = TodoList {items: vec![], file: None, path: None};
     let mut dir = env::current_dir().unwrap();
     let mut dir = dir.to_string_lossy().to_string();
     dir.push_str("/todo.json");
@@ -97,6 +142,7 @@ fn main() {
 
     if let Some(f) = find_todo(path) {
         todo.file = Some(f);
+        todo.path = Some(dir);
     }
 
     let args: Vec<String> = env::args().collect();
@@ -104,11 +150,6 @@ fn main() {
         panic!("Not enough arguments")
     }
 
-    for x in &args {
-        println!("Arg: {}", x);
-    }
-
-//instead need to check if todo.json exists
     if args[1] == String::from("init") {
         if todo.file.is_some() {
             println!("Already initialized!");
@@ -122,11 +163,11 @@ fn main() {
         panic!("Error: need to initialize todo.  Do todo init");
     }
     else {
-        todo.parse_args(args);
         println!("Parsing Arguments");
+        todo.parse_args(args);
     }
 }
 //todo add item optional<description> optional<-c> (complete)
 //todo complete id
 //todo remove id
-//todo show
+//todo show 
